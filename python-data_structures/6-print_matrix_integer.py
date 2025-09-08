7e7dde84737e3b315a8e814aa9036d006169f9a0
Emily Chew <emilychewsh@gmail.com>
2025-09-08 16:37:31 +1000

    without trailing whitespace

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for num in row:
-            print("{:d}".format(num), end=" ")
-        print()  # prints new line for each row
+        print(" ".join("{:d}".format(num) for num in row))
