14462d02448acd3afc032261de069b5ff1d0998a
Emily Chew <emilychewsh@gmail.com>
2025-09-12 13:40:39 +1000

    task 2

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    for i in range(x):
+        try:
+            if not isinstance(my_list[i], int):
+                pass
+            else:
+                print("{:d}".format(my_list[i]), end="")
+                count = count + 1
+        except IndexError:
+            print("list index out of range")
+    print()
+    return count
