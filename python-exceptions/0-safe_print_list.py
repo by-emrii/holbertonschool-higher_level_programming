16de7b3b57089ce9d22c9f357b7839618fc1295a
Emily Chew <emilychewsh@gmail.com>
2025-09-12 12:25:30 +1000

    task 0

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        count = 0
+        for i in range(x):
+            print(my_list[i], end="")
+            count = count + 1
+    except IndexError:
+        pass  # ignore if i goes beyond list length
+    print()
+    return count
