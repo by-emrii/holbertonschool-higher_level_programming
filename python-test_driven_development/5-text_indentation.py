26463092331736a862c49e153ee862877739a744
Emily Chew <emilychewsh@gmail.com>
2025-09-14 11:12:56 +1000

    task 5

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+This is the "text_indentation" module.
+
+The text_indentation module supplies one function, text_indentation().
+For example,
+
+text = "Hi. What's ur name?"
+>>> text_indentation(text)
+Hello.
+
+What's ur name?
+"""
+
+
+def text_indentation(text):
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    string = ""  # init empty string to add char to
+    for char in text:
+        string += char
+        if char in ".?:":
+            print(string.strip(), end="\n\n")
+            string = ""
+
+    if string:
+        print(string.strip(), end="")
