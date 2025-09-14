29658e360b0674a60c31d4a85a3278533128fcee
Emily Chew <emilychewsh@gmail.com>
2025-09-14 11:15:12 +1000

    pycodestyle

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -14,6 +14,10 @@ What's ur name?
 
 
 def text_indentation(text):
+    """
+    prints a text with 2 new lines after
+    each of these characters: ., ? and :
+    """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
