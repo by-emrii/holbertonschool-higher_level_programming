3a1d06607a08fdecc6dc04797c0f91368f5fcc7c
Emily Chew <emilychewsh@gmail.com>
2025-09-14 11:35:27 +1000

    required tests

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -7,19 +7,31 @@ max_integer = __import__('6-max_integer').max_integer
 class TestMaxInteger(unittest.TestCase):
 
     def test_max_int(self):
-        self.assertEqual(max_integer[1, 2, 3, 4], 4)
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
 
-    def test_negative_numbers(self):
-        self.assertEqual(max_integer([-10, -5, -1, -7]), -1)
+    def test_max_at_end(self):
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+
+    def test_max_at_beginning(self):
+        self.assertEqual(max_integer([4, 3, 2, 1]), 4)
+
+    def test_max_in_middle(self):
+        self.assertEqual(max_integer([1, 3, 4, 2]), 4)
+
+    def test_one_negative_number(self):
+        self.assertEqual(max_integer([1, 2, -3, 4]), 4)
+
+    def test_only_negative_numbers(self):
+        self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
     
     def test_single_element(self):
         self.assertEqual(max_integer([7]), 7)
     
-    def test_empty_list(self):
-        self.assertIsNone(max_integer([]))
-    
     def test_all_equal(self):
         self.assertEqual(max_integer([3, 3, 3, 3]), 3)
+    
+    def test_empty_list(self):
+        self.assertIsNone(max_integer([]))
 
 
 if __name__ == '__main__':
