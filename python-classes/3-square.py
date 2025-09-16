f35a8c498416dc4dfff25106b08fbaa48afaddd5
Emily Chew <emilychewsh@gmail.com>
2025-09-16 14:37:45 +1000

    task 3

---
+++ python-classes/3-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+This is the "3-square.py" class module.
+"""
+
+
+class Square:
+    """
+    Square with private size attr
+    """
+    def __init__(self, size=0):
+        """
+        Initialisation of a new instance from class Square
+
+        Args:
+            size (int): size of the square
+
+        Raises:
+            TypeError if instance is not an int
+            ValueError if size is a negative int
+        """
+
+        # check for errors first, then initialise
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    def area(self):
+        """
+        Calculate and return the area of the square
+        """
+        return self.__size**2
