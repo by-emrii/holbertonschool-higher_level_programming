8e12d63976538da692b1af42cbbc596d52dcbe0c
Emily Chew <emilychewsh@gmail.com>
2025-09-16 13:54:33 +1000

    task 0

---
+++ python-classes/0-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This is the "0-square.py" class module.
+"""
+
+
+class Square:
+    """
+    Initialisation of an instance of the class Square
+    """
+    def __init__(self):
+        pass
