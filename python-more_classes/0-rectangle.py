6338e11e1315dc6582977252039420205b685f20
Emily Chew <emilychewsh@gmail.com>
2025-09-17 12:49:28 +1000

    task 0

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This is the "0-rectangle" class module.
+"""
+
+
+class Rectangle:
+    def __init__(self):
+        """
+        Initialisation of object instance with in class Rectangle
+        """
+        pass
