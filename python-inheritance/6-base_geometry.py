76cd6e0ca890c17a6728237fa7d572914b458090
Emily Chew <emilychewsh@gmail.com>
2025-09-23 12:29:50 +1000

    task 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This is the "6-base_geometry" class module.
+"""
+
+
+class BaseGeometry:
+    """
+    BaseGeometry class
+    """
+    def area(self):
+        """
+        Raises an exception
+        """
+        raise Exception("area() is not implemented")
