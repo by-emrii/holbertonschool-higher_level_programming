61d4ad892afdb662ee7a0c8bd776a8c69befda14
Emily Chew <emilychewsh@gmail.com>
2025-09-29 15:06:24 +1000

    fix no new line

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -6,4 +6,4 @@ def read_file(filename=""):
     """ reads a file and prints content to stdout """
 
     with open(filename, "r", encoding="utf-8") as f:
-        print(f.read())
+        print(f.read(), end="")
