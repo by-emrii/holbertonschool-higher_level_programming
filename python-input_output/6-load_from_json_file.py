cfdab32590a685f55a03b38e7dae0222abdc1492
Emily Chew <emilychewsh@gmail.com>
2025-09-29 16:32:04 +1000

    task 6

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This is the "6-load_from_json_file.py" module.
+"""
+import json
+
+
+def load_from_json_file(filename):
+    """
+    function that creates an Object from a “JSON file”
+    """
+    with open(filename, "r", encoding="utf-8") as f:
+        return json.load(f)
