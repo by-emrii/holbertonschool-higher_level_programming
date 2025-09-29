970e3e0db8ea5be54b4b93ec73cb4e5bf918b27a
Emily Chew <emilychewsh@gmail.com>
2025-09-29 16:49:14 +1000

    task 7

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+This is the "7-add_item.py" module.
+Script that adds all arguments to a Python list,
+and then save them to a file
+"""
+import sys
+
+
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+
+filename = "add_item.json"
+
+try:
+    # if the file exists, load it
+    list = load_from_json_file(filename)
+except FileNotFoundError:
+    # otherwise, create new empty list
+    list = []
+
+# add all args, skipping script name
+list.extend(sys.argv[1:])
+
+# save the list back to the file
+save_to_json_file(list, filename)
