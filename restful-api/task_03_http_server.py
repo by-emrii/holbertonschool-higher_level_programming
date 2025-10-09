c45c13223d1a2c6aeb099f88ae98c79284fbce72
Emily Chew <emilychewsh@gmail.com>
2025-10-09 11:29:57 +1100

    task 3

---
+++ restful-api/task_03_http_server.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+This is the "task_03_http_server.py" module.
+"""
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
+
+
+class SimpleAPI(BaseHTTPRequestHandler):
+    """
+    Simple API which defines how to handle
+    GET requests
+    """
+    def do_GET(self):
+        if self.path == "/":
+            self.send_response(200)
+            self.send_header("Content-Type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"Hello, this is a simple API!")
+
+        elif self.path == "/data":
+            self.send_response(200)
+            self.send_header("Content-Type", "application/json")
+            self.end_headers()
+            data = {"name": "John", "age": 30, "city": "New York"}
+            self.wfile.write(json.dumps(data).encode())
+
+        elif self.path == "/status":
+            self.send_response(200)
+            self.send_header("Content-Type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"OK")
+
+        else:
+            self.send_response(404)
+            self.send_header("Content-Type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"404 Not Found")
+
+
+server = HTTPServer(("localhost", 8000), SimpleAPI)
+server.serve_forever()
