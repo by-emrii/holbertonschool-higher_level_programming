a9b42d0b931a1749173cdcdc85bdafd35a2dd5ac
Emily Chew <emilychewsh@gmail.com>
2025-10-23 16:45:13 +1100

    task 0

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" This is the "0-select_states.py" module """
+import MySQLdb
+import sys
+
+def list_all_states(username, password, database):
+    """ list all the states in the database """
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+if __name__ == "__main__":
+    list_all_states(sys.argv[1], sys.argv[2], sys.argv[3])
---
+++ python-object_relational_mapping/README.md
@@ -0,0 +1,17 @@
+This purpose of this directory is for Holberton School's Python Object-relational mapping project tasks <br>
+
+0. Get all states <br>
+1. Filter states <br>
+2. Filter states by user input <br>
+3. SQL Injection... <br>
+4. Cities by states <br>
+5. All cities by state <br>
+6. First state model <br>
+7. All states via SQLAlchemy <br>
+8. First state <br>
+9. Contains `a` <br>
+10. Get a state <br>
+11. Add a new state <br>
+12. Update a state <br>
+13. Delete states <br>
+14. Cities in state <br>
