a489aebeae67a813a776e7a60e44667f7a4b1171
Emily Chew <emilychewsh@gmail.com>
2025-10-23 16:54:07 +1100

    add binary

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -18,7 +18,7 @@ def list_all_states_with_name_starting_N(username, password, database):
     )
     cursor = db.cursor()
 
-    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    cursor.execute("SELECT * FROM states WHERE BINARY name LIKE 'N%' ORDER BY id ASC")
     rows = cursor.fetchall()
 
     for row in rows:
