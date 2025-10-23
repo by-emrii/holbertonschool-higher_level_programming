fe2eca5210f5dd1fb7a47ff4142aa09c71a1703a
Emily Chew <emilychewsh@gmail.com>
2025-10-23 12:24:07 +1100

    task 1

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,2 +1,4 @@
 --
-CREATE USER "user_0d_1"@"host" IDENTIFIED WITH authentication_plugin BY "user_0d_1_pwd";
+CREATE USER IF NOT EXISTS "user_0d_1"@"localhost" IDENTIFIED BY "user_0d_1_pwd";
+GRANT ALL PRIVILEGES ON *.* TO "user_0d_1"@"localhost" WITH GRANT OPTION;
+FLUSH PRIVILEGES;
