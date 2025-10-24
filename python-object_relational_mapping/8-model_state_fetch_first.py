33fd6eaea57df1dfe2681caa6c49a2234db3fe13
Emily Chew <emilychewsh@gmail.com>
2025-10-24 13:18:57 +1100

    task 8

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+lists all state objects from the database hbtn_0e_6_usa
+"""
+
+from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy import (create_engine)
+from model_state import State, Base
+import sys
+
+Base = declarative_base()
+
+if __name__ == "__main__":
+    # CLI arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    # create connection between sqlalchemy to mysql server
+    engine = create_engine(
+        f"mysql+mysqldb://{username}:{password}@localhost:3306/{database}",
+        pool_pre_ping=True
+        )
+
+    # bind engine to a session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # query first state
+    state = session.query(State).first()
+
+    # print results in format
+    print(f"{state.id}: {state.name}")
+
+    # close session
+    session.close()
