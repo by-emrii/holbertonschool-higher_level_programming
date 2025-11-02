b7a5be837dde1551d4c3eee1725c7108082dc9f7
Emily Chew <emilychewsh@gmail.com>
2025-11-02 21:23:09 +1100

    task 10

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const arg = parseInt(process.argv[2], 10);
+
+function factorial (num) {
+  if (isNaN(num) || num <= 1) {
+    return 1;
+  }
+  return num * factorial(num - 1);
+}
+
+console.log(factorial(arg));
